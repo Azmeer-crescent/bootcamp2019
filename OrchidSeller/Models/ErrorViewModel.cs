7323d96f4795b9b58f2e85dabab53af53b6d567f
azmeer <azmeerm.crescent@gmail.com>
2019-11-04 11:45:10 +0530

    init with asp.net

---
+++ OrchidSeller/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OrchidSeller.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
